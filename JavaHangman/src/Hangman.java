0781c38fa1f621c7e350c966ac95211de9629b43
Bradley Kettle <77495332+Bradoats@users.noreply.github.com>
2022-08-10 15:53:06 -0230

    2nd commit to the hangman project
    This is the 2nd commit to my hangman project, I added a new method called startGame() that takes the random word as an argument. Once you call this method, it creates a functional hangman game(even though the only hangman aspect is messages that tell you how many body parts he has) with exception handling(This is done with nested if/else statements that print appropriate "error messages" to the console. Next session I plan to actually throw proper exceptions with messages). The only major thing left to do with this project is implement the ACSII art, as well as clean up the code where possible.

--- JavaHangman/src/Hangman.java
+++ JavaHangman/src/Hangman.java
@@ -1,13 +1,12 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class Hangman {
     public static void main(String[] args) throws Exception {
-        String word = chooseWord();
-        System.out.println(word);
+        String randomWord = chooseWord();
+        startGame(randomWord);
     }
 
     public static String chooseWord() throws FileNotFoundException {
@@ -19,5 +18,49 @@ public class Hangman {
         String word = fileTokenized.get(rand.nextInt(fileTokenized.size()));
 
         return word;
+    }
+
+    public static void startGame(String word) {
+        Scanner sc = new Scanner(System.in);
+        int strikes = 0;
+        List<String> wordTokenized = List.of(word.split(""));
+        List<String> displayList = new ArrayList<String>();
+
+        List<String> alphabetList = new ArrayList<String>();
+        alphabetList.addAll(Arrays.asList("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","l","r","s","t","u","v","w","x","y","z"));
+
+        for(String letter : wordTokenized) {
+            displayList.add("_");
+        }
+
+        do {
+            System.out.println(displayList.toString());
+
+            System.out.println("Enter a letter: ");
+            String userInput = sc.next();
+
+            if(userInput.length() != 1) {
+                System.out.println("Your input can only have 1 character, please try again.");
+            } else {
+                if(alphabetList.contains(userInput) == false) {
+                    System.out.println("You used this character already, please try again.");
+                } else {
+                    if(wordTokenized.contains(userInput)) {
+                        displayList.set(wordTokenized.indexOf(userInput), userInput);
+                        //v code that removes a character from the alphabet array once it gets used v
+                        alphabetList.remove(alphabetList.indexOf(userInput));
+                    } else {
+                        strikes += 1;
+                        System.out.println("The hangman now has " + strikes + " body parts");
+                        //v code that removes a character from the alphabet array once it gets used v
+                        alphabetList.remove(alphabetList.indexOf(userInput));
+                    }
+                }
+
+                if(strikes == 6) {
+                    System.out.println("You lose, the word was " + word + ".");
+                }
+            }
+        } while(strikes != 6 || displayList.contains("_") == false);
     }
 }
